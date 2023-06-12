a4df316c73e7ca2a6d2f1f7979c231032b55565f
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-06-12 02:17:56 -0700

    weird name error fix

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const handleBarRoutes = require("./handleBarRoutes");
+const htmlRoutes = require("./htmlRoutes");
 
-router.use("/", handleBarRoutes);
+router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
